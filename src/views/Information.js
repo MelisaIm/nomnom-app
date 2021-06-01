583aa8d54983b485cbf7fa631c38354f8fb83ca4
Melisa Im <melisaevelynim@gmail.com>
2021-05-31 18:11:05 -0700

    Fix data fetching

--- src/views/Information.js
+++ src/views/Information.js
@@ -8,43 +8,42 @@ export default class Information extends React.Component {
         super(props);
         this.state = {
             "nom": {title: "Welcome to Nom Nom", summary: "Whether you have some health issues you don't know the cause of or are simply interested in changing up your diet, the aim of Nom Nom is to assist you in tracking your dietary choices and to track symptoms you may encounter. This is especially helpful for people who may have food sensitivities they may not realize they have. Good luck and happy eating!"},
-            "elimination diet": {title: "Elimination Diet", summary: "", content:{}, references: [], success: true, images: []},
-            "keto diet": {title: "Ketogenic Diet", summary: "", content:{}, references: [], success: true, images: []},
-            "food allergy": {title: "Food Allergy", summary: "", content:{}, references: [], success: true, images: []},
-            "food intolerance": {title: "Food Intolerance", summary: "", content:{}, references: [], success: true, images: []},
-            ibs: {title: "Food Allergy", summary: "", content:{}, references: [], success: true, images: []},
-            probiotics: {title: "Probiotics", summary: "", content:{}, references: [], success: true, images: []},
+            "elimination diet": {title: "Elimination Diet", content:{}, references: [], success: true, images: [], fetching: false},
+            "keto diet": {title: "Ketogenic Diet", content:{}, references: [], success: true, images: [], fetching: false},
+            "food allergy": {title: "Food Allergy", content:{}, references: [], success: true, images: [], fetching: false},
+            "food intolerance": {title: "Food Intolerance", content:{}, references: [], success: true, images: [], fetching: false},
+            ibs: {title: "Food Allergy", content:{}, references: [], success: true, images: [], fetching: false},
+            probiotics: {title: "Probiotics", content:{}, references: [], success: true, images: [], fetching: false},
             selected: "nom",
-            fetching: true,
         }
     }
 
     async componentDidMount() {
         let fetched = true; 
         const state = JSON.parse(window.localStorage.getItem('articles'));
-        for (const prop in state) {
-            if (prop !== "selected" && state[prop].summary === '') fetched = false;  
+        if (!state || !state["elimination diet"].summary || !state["keto diet"].summary || !state["food allergy"].summary 
+        || !state["food intolerance"].summary || !state.ibs.summary || !state.probiotics.summary) {
+            fetched = false;
         }
-        if (state !== null && !fetched) {
-            topics.forEach(topic => {
-                axios.get(`http://47.4.104.14:8000/scrape?search="${topic}"`)
-                .then((res) => {
-                    const data = res.data;
-                    this.setState((prevState) => ({[topic]: {
-                        ...prevState[topic],
-                        title: data.title,
-                        summary: data.summary,
-                        content: data["sub-sections"],
-                        references: data.references,
-                        images: data.images
-                    }}))
-                }).catch((err) => {
-                    this.setState((prevState) => ({[topic]: {...prevState[topic], success: false} }))
-                });  
+        if (state == null || !fetched) {
+            topics.forEach( async (topic) => {
+                this.setState(prevState => ({
+                    [topic]: {...prevState[topic], fetching: true}
+                }));
+                const {data} = await axios.get(`http://47.4.104.31:8000/scrape?search="${topic}"`);
+                this.setState((prevState) => ({
+                    [topic]: {...prevState[topic],
+                    title: data.title,
+                    summary: data.summary,
+                    content: data["sub-sections"],
+                    references: data.references,
+                    images: data.images,
+                    fetching: false
+                    }
+                }));
             });
-            this.setState({fetching: false});
         } else {
-            this.setState(state);
+            this.setState({...state, selected: "nom"});
         }
     }
     
@@ -61,10 +60,10 @@ export default class Information extends React.Component {
             <div className="view home">
                 <div className="top-container">
                     <div className="left-side">
-                    <div className="filter-buttons"><button onClick={() => this.handleClick("nom")}>Nom Nom</button>{topics.map((topic, index) => <button onClick={() => this.handleClick(topic)}key={index}>{this.state.fetching ? <><i className="fa fa-spinner fa-spin"></i> Loading</> :topic}</button>)}</div>
+                    <div className="filter-buttons"><button onClick={() => this.handleClick("nom")}>Nom Nom</button>{topics.map((topic, index) => <button onClick={() => !this.state[topic].fetching && this.handleClick(topic)}key={index}>{this.state[topic].fetching ? <><i className="fa fa-spinner fa-spin"></i> Loading</> :topic}</button>)}</div>
                         <div className="text-container">
                         <h4>{this.state[this.state.selected].title}</h4>
-                        <div className="image-container">{!this.state.fetching && this.state[this.state.selected].images && this.state[this.state.selected].images.map((image, index) => index < 2 && <img src={image}></img>)}</div>
+                        <div className="image-container">{!this.state[this.state.selected].fetching && this.state[this.state.selected].images && this.state[this.state.selected].images.map((image, index) => index < 2 && <img key={index} src={image}></img>)}</div>
                         {this.state[this.state.selected].summary}
                         </div>
                     </div>
